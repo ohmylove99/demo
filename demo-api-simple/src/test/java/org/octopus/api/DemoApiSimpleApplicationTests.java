673041326778bc8f1a0d157570cfb7134bae7076
Joshua Leng <joshualeng@192.168.0.106>
2020-01-27 13:24:50 +0800

    init

---
+++ demo-api-simple/src/test/java/org/octopus/api/DemoApiSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.octopus.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
